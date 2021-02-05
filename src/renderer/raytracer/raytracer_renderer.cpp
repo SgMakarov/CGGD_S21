1e678b3fe86408b3f8dae3ab811983131bb2958f
SgMakarov <s.makarov@innopolis.university>
2021-02-05 23:52:28 +0300

    Add some more raytracing code

--- src/renderer/raytracer/raytracer_renderer.cpp
+++ src/renderer/raytracer/raytracer_renderer.cpp
@@ -8,8 +8,14 @@ void cg::renderer::ray_tracing_renderer::init()
 	render_target = std::make_shared<cg::resource<cg::unsigned_color>>(
 		settings->width, settings->height);
 	raytracer = std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>() ;
+	shadow_raytracer =
+		std::make_shared<cg::renderer::raytracer<cg::vertex, cg::unsigned_color>>();
 	raytracer->set_render_target(render_target);
 	raytracer->set_viewport(settings->width, settings->height);
+	model = std::make_shared<cg::world::model>();
+	model->load_obj(settings->model_path);
+	raytracer->set_per_shape_vertex_buffer(model->get_per_shape_buffer());
+
 
 	camera = std::make_shared<cg::world::camera>();
 	camera->set_height(static_cast<float>(settings->height));
@@ -25,11 +31,14 @@ void cg::renderer::ray_tracing_renderer::init()
 	camera->set_z_far(settings->camera_z_far);
 	camera->set_z_near(settings->camera_z_near);
 
+
+	lights.push_back({ float3{ 0, 1.58f, -0.03f }, float3{ 0.78f, 0.78f, 0.78f } });
 }
 
 void cg::renderer::ray_tracing_renderer::destroy() {}
 
 void cg::renderer::ray_tracing_renderer::update() {}
+
 
 void cg::renderer::ray_tracing_renderer::render()
 {
@@ -43,6 +52,41 @@ void cg::renderer::ray_tracing_renderer::render()
 		};
 		return payload;
 	};
+	shadow_raytracer->miss_shader = [](const ray& ray) {
+		payload payload{};
+		payload.t - -1.f;
+		return payload;
+	};
+	shadow_raytracer->any_hit_shader = [](const ray& ray, payload& payload,
+										  const triangle<cg::vertex>& triangle) {
+		return payload;
+	};
+	raytracer->closest_hit_shader = [&](const ray& ray, payload& payload,
+									   const triangle<cg::vertex>& triangle) {
+		float3 result_color = triangle.emissive;
+		float3 position = ray.position + ray.direction * payload.t;
+		float3 normal = payload.bary.x * triangle.na +
+						payload.bary.y * triangle.nb + payload.bary.z * triangle.nc;
+		for (auto& light : lights)
+		{
+			cg::renderer::ray to_light(position, light.position - position);
+			auto shadow_payload =
+				shadow_raytracer->trace_ray(to_light, 1, length(light.position - position));
+			if (shadow_payload.t == -1.f)
+			{
+				result_color += triangle.diffuse * light.color *
+								std::max(dot(normal, to_light.direction), 0.f);
+				
+			}
+		}
+		payload.color = cg::color::from_float3(result_color);
+		return payload;
+	};
+	shadow_raytracer->acceleration_structures = raytracer->acceleration_structures;
+
+
+	raytracer->build_acceleration_structure();
+
 	raytracer->ray_generation(
 		camera->get_position(), camera->get_direction(), camera->get_right(),
 		camera->get_up());
